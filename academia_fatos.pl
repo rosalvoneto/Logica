5c696acf8b0749e962a73701a0595fae202a62c2
Rosalvo Neto <rosalvo.neto@gmail.com>
2019-12-09 15:36:20 -0300

    Add files via upload

---
+++ academia_fatos.pl
@@ -0,0 +1,32 @@
+% FATOS
+feminino(ana).
+feminino(bia).
+feminino(eva).
+feminino(lia).
+
+masculino(ivo).
+masculino(ary).
+
+idade(ana, 23).
+idade(bia, 19).
+idade(ivo, 22).
+idade(lia, 17).
+idade(eva, 28).
+idade(ary, 25).
+
+
+altura(ana, 1.55).
+altura(bia, 1.71).
+altura(ivo, 1.80).
+altura(lia, 1.85).
+altura(eva, 1.75).
+altura(ary, 1.72).
+
+peso(ana, 56).
+peso(bia, 61.3).
+peso(ivo, 70.5).
+peso(lia, 57.3).
+peso(eva, 68.7).
+peso(ary, 68.9).
+
+
